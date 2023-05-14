c21f4d15cd629ea29bb0ab240e93b7d0641c3e2f
e-az1z <ergashevazizjon189@gmail.com>
2023-05-15 01:30:08 +0500

    darslar

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	echo "assalomu aleykum";
-
- ?>
---
+++ uy_ishi/index.php
@@ -0,0 +1,94 @@
+<?php
+
+	/* $number = 20.6;
+	$result = gettype($number);
+	$natija = var_dump($result);
+	echo $natija; */
+	# var_dump() funcsiyasiga misol
+
+
+	// $ism = "Aziz";
+	// echo gettype($ism);
+	# gettype() funcsiyasiga misol
+
+
+	// $ism_familya = "Aziz Ergashev";
+	// echo is_string($ism_familya);
+	# is_string funcsiyasiga misol natijaga 1 chiqadi bu javascript va php dasturlash tillarida to'g'ri degan manoni anglatadi 0 xato 1 to'g'ri
+
+
+	// $num = "son";
+	// echo is_integer($num);
+	# is_integer() funcsiyasiga misol
+
+
+	// $float = 15.5;
+	// echo is_float($float);
+	# is_float() funcsiyasiga misol
+
+
+	// $bool = false;
+	// echo gettype(is_bool($bool));
+	# is_bool() funcsiyasiga misol
+
+
+	// $_null = NULL;
+	// echo is_null($_null);
+	# bo'sh o'rgaruvchi hech qanday qiymatga ega emas.
+
+
+
+
+	# topshiriq 3
+
+	// $num_1 = 729;
+	// $num_2 = 500;
+	// $num_3 = 596;
+	// $num_4 = 877;
+	// $num_5 = 1500;
+
+ ?>
+
+<!--  <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 	<title>Document</title>
+ 	<style>
+ 		tr {
+ 			text-align: center;
+ 		}
+ 	</style>
+ </head>
+ <body>
+
+ 	<table border="1">
+
+  		<tbody>
+  			<tr>
+  				<td>Aziz</td>
+  				<td rowspan="1"><?php echo $num_1; ?></td>
+  			</tr>
+  			<tr>
+  				<td>Muhammadalli</td>
+  				<td rowspan="1"><?php echo $num_2; ?></td>
+  			</tr>
+  			<tr>
+  				<td>Abbos</td>
+  				<td rowspan="1"><?php echo $num_3 ?></td>
+  			</tr>
+  			<tr>
+  				<td>Fayoz</td>
+  				<td rowspan="1"><?php echo $num_4; ?></td>
+  			</tr>
+  			<tr>
+  			    <td>Shoxjahon</td>
+  			    <td rowspan="1"><?php echo $num_5; ?></td>
+  			</tr>
+  		</tbody>
+
+    </table>
+
+ </body>
+ </html> -->
