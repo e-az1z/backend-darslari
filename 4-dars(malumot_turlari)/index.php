e8b77254d61d3d5598248cf73f2bac6779366065
e-az1z <ergashevazizjon189@gmail.com>
2023-05-15 01:22:29 +0500

    uy ishi

---
+++ 4-dars(malumot_turlari)/index.php
@@ -0,0 +1,31 @@
+<?php
+
+	// echo "Asssalomu aleykum";
+	# echo "Asssalomu aleykum";
+	// echo "Asssalomu aleykum </br>";
+	// echo "<h1>Asssalomu aleykum</h1>";
+
+	// $_matn = "oddiy matn";
+
+	// echo "o'zgaruvchi = $_matn</br>";
+	// echo "o'zgaruvchi = {$_matn}";
+
+	$matn = "Assalomu aleykum"; // string type
+
+	$son = 20; // integer(butun son) turi
+
+	$float = 20.5; // float turi
+
+	$active = true; // boolen(bool) turi
+
+	$_null = NULL; //  bo'sh o'rgaruvchi hech qanday qiymatga ega emas.
+
+	// var_dump($_null)
+
+	$result = var_dump(is_null($_null));
+
+	echo $result;
+
+ ?>
+
+ <!-- <pre><?= $result; ?></pre> -->
